7df7cb6aa904e562351ae55d211624b0634c79d0
Patricia Carter <patricia4194@gmail.com>
2018-08-18 16:08:44 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -16,7 +16,7 @@ def run_guessing_game
     case input
       when "exit"
         puts "Goodbye!"
-        break
+        
       when random == input.to_i
         puts "You guessed the correct numbr!"
       else
