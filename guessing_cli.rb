4c0099855f875137dde4227d798230355d77a7fc
Patricia Carter <patricia4194@gmail.com>
2018-08-18 15:45:50 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,9 +1,13 @@
 def run_guessing_game
   # Generate a random number
   
-  
+  random = rand(1..6)
   
   # Take input from the command line
+  
+  puts "Guess a number between 1 and 6"
+  
+  
   # Compare the user's number to the random number
   # If the number as been guessed correctly, print correct
   # If not, print the computer's answer
