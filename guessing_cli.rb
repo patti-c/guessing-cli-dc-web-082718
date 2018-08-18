6a363ca49af054871d3f75e8ebc3a00785357c7f
Patricia Carter <patricia4194@gmail.com>
2018-08-18 15:59:19 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,23 +2,23 @@ def run_guessing_game
   
   while true
   
-  # Generate a random number
-  
-  random = rand(1..6)
-  
-  # Take input from the command line
-  
-  puts "Guess a number between 1 and 6"
-  input = gets.chomp
-  
-  # Compare the user's number to the random number
-  
-  if random == input
-    puts "You guessed the correct numbr!"
-  elsif 
-  
-  # If the number as been guessed correctly, print correct
-  # If not, print the computer's answer
-  # If exit was the input, exit the program
+    # Generate a random number
+    
+    random = rand(1..6)
+    
+    # Take input from the command line
+    
+    puts "Guess a number between 1 and 6"
+    input = gets.chomp
+    
+    # Compare the user's number to the random number
+    
+    if random == input
+      puts "You guessed the correct numbr!"
+    elsif 
+    
+    # If the number as been guessed correctly, print correct
+    # If not, print the computer's answer
+    # If exit was the input, exit the program
   
 end
