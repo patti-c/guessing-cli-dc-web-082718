dd494f0ef2f3769183b93ba1d850839e98c68f6e
Patricia Carter <patricia4194@gmail.com>
2018-08-18 16:20:05 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
   
-  #while true
+  while true
   
     # Generate a random number
     
@@ -21,6 +21,6 @@ def run_guessing_game
         puts "The computer guessed #{random}"
     end
     
-  #end 
+  end 
   
 end
