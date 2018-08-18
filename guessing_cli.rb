edcae3cfc0f336cee563b8c42f4bd375cca28888
Patricia Carter <patricia4194@gmail.com>
2018-08-18 15:43:03 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,8 +1,12 @@
 def run_guessing_game
   # Generate a random number
+  
+  
+  
   # Take input from the command line
   # Compare the user's number to the random number
   # If the number as been guessed correctly, print correct
-  # If not, 
+  # If not, print the computer's answer
+  # If exit was the input, exit the program
   
 end
