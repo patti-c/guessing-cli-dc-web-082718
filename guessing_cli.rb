94fcc970fe888b2a89450ca13031e6a14e2470ca
Patricia Carter <patricia4194@gmail.com>
2018-08-18 15:40:44 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,3 +1,8 @@
 def run_guessing_game
+  # Generate a random number
+  # Take input from the command line
+  # Compare the user's number to the random number
+  # If the number as been guessed correctly, print correct
+  # If not, 
   
 end
